2b12929a99daa27231e02427b59c8ea14aa14670
Mendy <MendyZ@gmail.com>
2017-01-09 16:45:59 -0600

    first commit with two games, one as an html export and two as construct 2 files with initial working games, and then those two as extended versions meant for me to play around with

---
+++ FirstGameHTML5/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1483810294,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/orangebackground-sheet0.png",
+		"images/orangebackground-sheet1.png",
+		"images/orangebackground-sheet2.png",
+		"images/ground-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/player-sheet0.png",
+		"images/spike-sheet0.png",
+		"images/hudbackground-sheet0.png",
+		"images/btnhome-sheet0.png",
+		"images/btnplayagain-sheet0.png",
+		"images/btnplay-sheet0.png",
+		"images/btnmoregames-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
