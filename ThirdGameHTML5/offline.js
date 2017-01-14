24b68bac651ae04d3d617cc58af279ca53acc7e1
Mendy <MendyZ@gmail.com>
2017-01-13 19:29:55 -0600

    the third game creation of spinner shooter basic game logic created. Uploaded as well and works through the sever.

---
+++ ThirdGameHTML5/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1484356984,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/background-sheet0.png",
+		"images/background-sheet1.png",
+		"images/background-sheet2.png",
+		"images/player-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/projectile-sheet0.png",
+		"images/spawnlocation0-sheet0.png",
+		"images/blackparticles.png",
+		"images/whiteparticles.png",
+		"images/hudbackground-sheet0.png",
+		"images/btnhome-sheet0.png",
+		"images/btnagain-sheet0.png",
+		"images/btnplay-sheet0.png",
+		"images/btnmoregames-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
