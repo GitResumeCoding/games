1cc9be381627e4c9e0d20be01923fffa657cd574
Mendy <MendyZ@gmail.com>
2017-01-23 15:44:22 -0600

    new snag me some color game, new menu index page

--- snagMeSomeColor/snagMeSomeColorHTML5/offline.js
+++ snagMeSomeColor/snagMeSomeColorHTML5/offline.js
@@ -1,14 +1,14 @@
 ﻿{
-	"version": 1484698525,
+	"version": 1484714027,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"images/background-sheet0.png",
 		"images/player-sheet0.png",
-		"images/enemygreen-sheet0.png",
-		"images/enemyorange-sheet0.png",
-		"images/enemyblue-sheet0.png",
+		"images/coinpoints-sheet0.png",
+		"images/bomb-sheet0.png",
+		"images/mushroomhealthup-sheet0.png",
 		"images/projectile-sheet0.png",
 		"images/deadblock-sheet0.png",
 		"images/sprite-sheet0.png",
