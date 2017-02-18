2181e27cee6d4fea317b227a7e034354a658a956
Mendy <MendyZ@gmail.com>
2017-02-18 11:30:12 -0600

    Garbage collection game and menu added

---
+++ GarbageCollector/GarbageCollectorHTML5/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1487438324,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/background-sheet0.png",
+		"images/collectibleorange-sheet0.png",
+		"images/goal-sheet0.png",
+		"images/collectiblebadblue-sheet0.png",
+		"images/btnplay-sheet0.png",
+		"images/particlesorange.png",
+		"images/particlesblue.png",
+		"images/backgroundhome-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
